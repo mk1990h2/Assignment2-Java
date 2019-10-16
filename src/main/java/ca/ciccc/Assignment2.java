64514336feec357033c9152c1a24236e1daad142
Mika <mikass111@yahoo.co.jp>
2019-10-16 15:22:38 -0700

    Revert "Complete assignment2"
    This reverts commit 5a08c938e180c063583ea050aff92d7abef6ab3b.

--- src/main/java/ca/ciccc/Assignment2.java
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -9,146 +9,90 @@ package ca.ciccc;
 public class Assignment2 {
     /**
      * Write nested for-loops to produce the following output from the given input n.
-     *
      * @param n number to create a pattern.
      * @return pattern
-     * <p>
+     *
      * Sample Input / Output
      * numberLoops(3);
-     * <p>
+     *
      * Sample Output
      * ..1
      * .22
      * 333
+     *
      */
     public String numberLoops(int n) {
-        String result = "";
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (j > n - i) {
-                    result += String.valueOf(i);
-                } else {
-                    result += ".";
-                }
-            }
-            if (i != n) {
-                result += "\n";
-            }
-        }
+        // Todo: your code goes here
 
-        return result;
+        return null;
     }
 
     /**
      * Given an array of integers, return true if the number of 1s is greater than the number of 7s
-     *
      * @param nums array of ints
      * @return true if the number of 1s is greater than the number of 7s
      */
     public boolean countNumbers(int[] nums) {
-        int count1 = 0;
-        int count7 = 0;
-        for (int num : nums) {
-            if (num == 1) {
-                count1++;
-            }
-            if (num == 7) {
-                count7++;
-            }
-        }
+        // Todo: your code goes here
 
-        return count1 > count7;
+        return false;
     }
 
     /**
      * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
      * is very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
      * does not count.
-     *
      * @param nums array of numbers
      * @return the sum except for 13
-     * <p>
+     *
      * Sample Input / Output
      * sumExcept13({1, 2, 2, 1}) -> 6
      * sumExcept13({1, 2, 2, 1, 13} -> 6
      * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
      */
     public int sumExcept13(int[] nums) {
-        int sum = 0;
-        int before = 0;
-        for (int num : nums) {
-            if (num != 13 && before != 13) {
-                sum += num;
-            }
-            before = num;
-        }
+        // Todo: your code goes here
 
-        return sum;
+        return 0;
     }
 
     /**
      * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}.
      * You may modify and return the given array, or return a new array.
-     *
      * @param nums array of ints
      * @return shifted array
-     * <p>
+     *
      * Sample Input / Output
      * shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6}
      * shiftArray({2, 3}) -> {3, 2}
      * shiftArray({7}) -> {7}
      */
     public int[] shiftArray(int[] nums) {
-        int[] result = new int[nums.length];
-        if (nums.length != 0) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                result[i] = nums[i + 1];
-            }
-            result[nums.length - 1] = nums[0];
-        }
+        // Todo: your code goes here
 
-        return result;
+        return null;
     }
 
     /**
      * Return true if the array contains three increasing adjacent numbers, otherwise false.
-     *
      * @param nums array of ints.
      * @return true if the array contains three increasing adjacent numbers, otherwise false.
      */
     public boolean tripleIncreasingOrder(int[] nums) {
-        boolean hasTripleIncreasing = false;
-        if (nums.length >= 3) {
-            for (int i = 0; i + 2 < nums.length; i ++) {
-                if (nums[i] + 1 == nums[i + 1]
-                        && nums[i + 1] + 1 == nums[i + 2]) {
-                    hasTripleIncreasing = true;
-                    break;
-                }
-            }
-        }
+        // Todo: your code goes here
 
-        return hasTripleIncreasing;
+        return false;
     }
 
     /**
      * Return true if the array contains the same number of odds and even numbers, otherwise false.
-     *
      * @param nums array of ints.
      * @return true if the array contains the same number of odds and even numbers, otherwise false.
      */
-    public boolean evenOrOdd(int[] nums) {
-        int oddCount = 0;
-        int evenCount = 0;
-        for (int num : nums) {
-            if(num % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
-            }
-        }
+    public boolean evenOrOdd(int[] nums){
+        // Todo: your code goes here
 
-        return evenCount == oddCount;
+        return false;
     }
 }
 
--- src/main/java/ca/ciccc/MagicSquare.java
+++ src/main/java/ca/ciccc/MagicSquare.java
@@ -1,11 +1,12 @@
 package ca.ciccc;
 
 /**
+ *
  * Magic Squares are square arrays of numbers that have the interesting property that
  * the numbers in each column, and in each row, all add up to the same total.
- * <p>
+ *
  * Given an n x n square of numbers, determine if it is magic square.
- * <p>
+ *
  * Reference: {@link java.lang.Integer}
  * Integer is a wrapper class for {@code int} primitive type.
  */
@@ -17,7 +18,6 @@ public class MagicSquare {
 
     /**
      * Constructor that takes n as the number of rows and cols.
-     *
      * @param n number of rows and cols.
      */
     public MagicSquare(int n) {
@@ -27,8 +27,7 @@ public class MagicSquare {
     /**
      * Constructor that takes n as the number of rows and cols.
      * Initializes square 2-Dimensional array.
-     *
-     * @param n      number of rows and cols.
+     * @param n number of rows and cols.
      * @param square
      */
     public MagicSquare(int n, Integer[][] square) {
@@ -38,7 +37,6 @@ public class MagicSquare {
 
     /**
      * Getter for square
-     *
      * @return {@code this.square}
      */
     public Integer[][] getSquare() {
@@ -47,7 +45,6 @@ public class MagicSquare {
 
     /**
      * Setter for square.
-     *
      * @param square
      */
     public void setSquare(Integer[][] square) {
@@ -56,63 +53,29 @@ public class MagicSquare {
 
     /**
      * Check if {@code square} is magic square or not.
-     *
      * @param square 2-Dimensional array
      * @return {@code true} if {@code square} is magic, otherwise {@code false}
-     * <p>
+     *
      * Sample Input
      * {
-     * {16, 3, 2, 13},
-     * {5, 10, 11, 8},
-     * {9, 6, 7, 12},
-     * {4, 15, 14, 1}
+     *   {16, 3, 2, 13},
+     *   {5, 10, 11, 8},
+     *   {9, 6, 7, 12},
+     *   {4, 15, 14, 1}
      * }
-     * <p>
+     *
      * Sample Output
      * true
-     * <p>
+     *
      * Explanation: The sums of each row and col are equal.
      */
     public static boolean isMagicSquare(Integer[][] square) {
-        // Check the sum of each rows
-        boolean magicSquare = true;
-        int base = 0;
-        for (Integer num : square[0]) {
-            base += num;
-        }
-        for (int i = 1; i < square.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < square[i].length; j++) {
-                sum += square[i][j];
-            }
-            if (sum != base) {
-                magicSquare = false;
-                break;
-            }
-        }
-
-        // Check the sum of columns
-        int row = square.length;
-        int column = square[0].length;
-        if (magicSquare) {
-            for (int i = 0; i < column; i++) {
-                int sum = 0;
-                for (int j = 0; j < row; j++) {
-                    sum += square[j][i];
-                }
-                if (sum != base) {
-                    magicSquare = false;
-                    break;
-                }
-            }
-        }
-
-        return magicSquare;
+        // TODO: You need to implement this method.
+        return false;
     }
 
     /**
      * Check if {@code this.square} is magic square or not.
-     *
      * @return {@code true} if {@code square} is magic, otherwise {@code false}
      */
     public boolean isMagicSquare() {
